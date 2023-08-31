468afd41861b665d1ec352e85ca3906dcb73f758
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2023-08-31 18:49:12 +0330

    initial commit

---
+++ BotConfig.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+
+Api_id = ''
+Api_hash = ''
+bot_token = ''
+
+Bot = Client(
+    name="ApiHashApiId",
+    api_hash=Api_hash,
+    api_id=Api_id,
+    bot_token=bot_token
+)
